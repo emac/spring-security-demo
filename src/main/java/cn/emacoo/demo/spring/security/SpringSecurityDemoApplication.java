da7a356568984e86945040eedec5b01a581f847d
Emac <shen.bin.1983@gmail.com>
2019-07-24 10:29:23 +0800

    add cors

--- src/main/java/cn/emacoo/demo/spring/security/SpringSecurityDemoApplication.java
+++ src/main/java/cn/emacoo/demo/spring/security/SpringSecurityDemoApplication.java
@@ -6,7 +6,10 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,9 +31,15 @@ public class SpringSecurityDemoApplication implements WebMvcConfigurer {
         registry.addViewController("/basic").setViewName("basic/home");
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
     @Controller
     public static class FormController {
 
+        @CrossOrigin(origins = "http://localhost:8080", methods = RequestMethod.GET)
         @GetMapping("/form")
         @Secured("ROLE_ADMIN")
         public String home(Map<String, Object> model) {
