1faf95823021b1abfa77e7b3d433224bc93975ac
Emac <shen.bin.1983@gmail.com>
2019-09-29 01:15:21 +0800

    add requestMatcher

--- src/main/java/cn/emacoo/demo/spring/security/FormSecurityConfig.java
+++ src/main/java/cn/emacoo/demo/spring/security/FormSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * @author Emac
@@ -17,6 +18,7 @@ public class FormSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(new AntPathRequestMatcher("/form/**"));
         http.antMatcher("/form/**")
                 .authorizeRequests()
                 .antMatchers("/form/login").anonymous()
--- src/main/java/cn/emacoo/demo/spring/security/HttpBasicSecurityConfig.java
+++ src/main/java/cn/emacoo/demo/spring/security/HttpBasicSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
  * @author Emac
@@ -15,6 +16,7 @@ public class HttpBasicSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requestMatcher(new AntPathRequestMatcher("/basic/**"));
         http.antMatcher("/basic/**")
                 .authorizeRequests().anyRequest().authenticated()
                 .and().httpBasic();
